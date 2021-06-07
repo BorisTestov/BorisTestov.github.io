52609f2e037f17d37ce308a8e434292b5853a1e0
testov <testov@protei.ru>
2021-06-07 11:52:36 +0300

    Personal info updated
    Update personal info and CV

--- js/init.js
+++ js/init.js
@@ -505,7 +505,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 
 		animateSpan.typed({
-			strings: ["Deep Learning Specialist", "Python Developer", "Reverse Engineer"],
+			strings: ["C++ Developer", "Python Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
