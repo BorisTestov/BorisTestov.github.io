135e2553eac350432609c818c7b8859bfc8cac79
BorisTestov <boris.testov@gmail.com>
2023-03-04 16:48:27 +0200

    Fix filename

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("service_worker.js").then(registration=>{
-      console.log("Service Worker Registered!");
-    }).catch(error=>{
-      console.log("Service Workker Registration Failed");
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service_worker.js');
     });
-}else{
-  console.log("Not supported");
 }
