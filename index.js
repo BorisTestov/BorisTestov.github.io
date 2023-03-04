62e566d407feeacbccfd95b19318202661cf6eef
BorisTestov <boris.testov@gmail.com>
2023-03-04 17:02:36 +0200

    Fix PWA

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service_worker.js');
+    navigator.serviceWorker.register('/boristestov.github.io/service_worker.js', {scope: '/boristestov.github.io/'});
     });
 }
