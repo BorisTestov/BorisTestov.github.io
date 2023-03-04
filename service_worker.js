b2c9fea650d413fdd8a30476b7d9c1eea185c538
BorisTestov <boris.testov@gmail.com>
2023-03-04 17:15:25 +0200

    Fix PWA

--- service_worker.js
+++ service_worker.js
@@ -1,16 +1,53 @@
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open("static").then(cache=>{
-      return cache.addAll(["./",'./img/icon/android-chrome-192x192.png']);
-    })
-  );
-});
+var GHPATH = '/boristestov.github.io';
+var APP_PREFIX = 'borisPWA';
+var VERSION = '0.1';
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/css/`,
+  `${GHPATH}/resources/`,
+  `${GHPATH}/img/`,
+  `${GHPATH}/js/`
+]
 
-// Fatch resources
-self.addEventListener("fetch",e=>{
+var CACHE_NAME = APP_PREFIX + VERSION
+self.addEventListener('fetch', function (e) {
+  console.log('Fetch request : ' + e.request.url);
   e.respondWith(
-    caches.match(e.request).then(response=>{
-      return response||fetch(e.request);
+    caches.match(e.request).then(function (request) {
+      if (request) { 
+        console.log('Responding with cache : ' + e.request.url);
+        return request
+      } else {       
+        console.log('File is not cached, fetching : ' + e.request.url);
+        return fetch(e.request)
+      }
     })
-  );
-});
+  )
+})
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Installing cache : ' + CACHE_NAME);
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('Deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
